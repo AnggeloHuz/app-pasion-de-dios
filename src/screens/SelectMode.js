f81e19e0606db67a735414fe4d4e48730bafb348
AnggeloHuz <huzanggelo0904@gmail.com>
2025-12-10 13:56:33 -0400

    Home en modo claro listo

--- src/screens/SelectMode.js
+++ src/screens/SelectMode.js
@@ -1,39 +1,64 @@
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from 'react'; // No olvides importar React
+import { View, Text, StyleSheet, Button, Image, Dimensions, TouchableOpacity } from 'react-native';
 import ScreenWrapper from '../components/ScreenWrapper.js';
 
+const { width } = Dimensions.get("window");
+
 const SelectMode = ({ navigation }) => {
+  const shadowStyle = {
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  };
+
   return (
-    <ScreenWrapper>
-      <View style={styles.container}>
-        <Text style={styles.text}>Selecciona el modo</Text>
-        <Text>Aquí irían las opciones de modo...</Text>
-        <Button
-          title="Modo Cantante"
-          // Usamos navigation.navigate con el nombre de la ruta definido abajo
-          onPress={() => navigation.navigate("Cantante")}
+    <ScreenWrapper statusBarBg="#F49404" statusBarColor="#fff">
+      <View className="flex-1 bg-white">
+        <Image
+          source={require("../../assets/VectorA.png")}
+          className="w-full"
+          resizeMode="stretch"
         />
-        <Button
-          title="Modo Musico"
-          // Usamos navigation.navigate con el nombre de la ruta definido abajo
-          onPress={() => navigation.navigate("Musico")}
+
+        {/* 2. EL CONTENIDO: En su propia caja centrada */}
+        <View className="flex-1 items-center justify-center px-5">
+          <Text className="text-3xl font-medium italic ">App Musical</Text>
+          <Image
+            source={require("../../assets/logoapp.png")}
+            className="w-64 h-64"
+            resizeMode="stretch"
+          />
+
+          <TouchableOpacity
+            className="w-full bg-amarillo p-4 rounded-xl mb-6 items-center shadow-md"
+            style={shadowStyle}
+            onPress={() => navigation.navigate("Cantante")}
+          >
+            <Text className="text-white font-bold text-lg">
+              🎤 Modo Cantante
+            </Text>
+          </TouchableOpacity>
+
+          {/* Botón 2: Modo Músico (Estilo Bordeado/Outline) */}
+          <TouchableOpacity
+            className="w-full bg-amarillo p-4 rounded-xl mb-6 items-center"
+            style={shadowStyle}
+            onPress={() => navigation.navigate("Musico")}
+          >
+            <Text className="text-white font-bold text-lg">🎸 Modo Músico</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Image
+          source={require("../../assets/VectorB.png")}
+          className="w-full"
+          resizeMode="stretch"
         />
       </View>
     </ScreenWrapper>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  }
-});
 
 export default SelectMode;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   // Aquí le decimos que busque en App.js y en TODA la carpeta src
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "naranja": "#F49404",
+        "amarillo": "#FBC247",
+        "rojo": "#AB1B14"
+      }
+    },
   },
   plugins: [],
 }
